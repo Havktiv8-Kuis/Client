04fe8785e5e79af8d29aeadf7e6211b6145525f2
ditobprasetio <ditobagus28@gmail.com>
2020-04-16 20:33:27 +0700

    minor fix

--- type_client/src/views/GameBoard.vue
+++ type_client/src/views/GameBoard.vue
@@ -3,31 +3,8 @@
     <div class="game-board">
       <div class="row">
         <!-- ini cardnya buat di looping -->
-        <div class="card">
-          <img src="../assets/user.svg" alt="user-image">
-          <h3>Player 1</h3>
-          <h5>Score:</h5>
-          <p>70</p>
-        </div>
+        <Card/>
         <!-- -------- -->
-        <div class="card">
-          <img src="../assets/user.svg" alt="user-image">
-          <h3>Player 2</h3>
-          <h5>Score:</h5>
-          <p>50</p>
-        </div>
-        <div class="card">
-          <img src="../assets/user.svg" alt="user-image">
-          <h3>Player 3</h3>
-          <h5>Score:</h5>
-          <p>10</p>
-        </div>
-        <div class="card">
-          <img src="../assets/user.svg" alt="user-image">
-          <h3>Player 4</h3>
-          <h5>Score:</h5>
-          <p>30</p>
-        </div>
       </div>
       <div class="text-box">
         <p>
@@ -49,8 +26,12 @@
 </template>
 
 <script>
+import Card from '../components/Card'
 export default {
-  name: 'GameBoard'
+  name: 'GameBoard',
+  components: {
+    Card
+  }
 }
 </script>
 
@@ -84,42 +65,6 @@ export default {
   font-size: 25px;
   text-align: left;
   color: #fce669;
-}
-
-.card{
-  width: 12rem;
-  height: 100%;
-  padding: 10px;
-  display: flex;
-  flex-direction: column;
-  justify-content: space-around;
-  align-items: center;
-  border: 3px ;
-  border-style: solid;
-  border-color: #ffc150;
-  border-radius: 20px;
-  margin-right: 30px;
-  background: #fce669;
-}
-
-.card img{
-  width: 100px;
-  margin-bottom: 15px;
-}
-
-.card h3{
-  color: #cb4a39;
-}
-
-.card h5{
-  margin: 0;
-  color: #9e6c36;
-}
-
-.card p{
-  font-size: 4rem;
-  margin: 0;
-  color: #774b30;
 }
 
 </style>
